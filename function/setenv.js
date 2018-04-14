3ba3ff8ae8a0633584a0d4e02aaafd05ce9fc1d2
zhoushoujian <qianchengyimeng@qq.com>
2018-04-14 15:51:19 +0800

    精简代码

--- function/setenv.js
+++ function/setenv.js
@@ -2,29 +2,27 @@
 var item_reg = /^\s*set\s+([\S]+)\s*=([\s\S]*)$/im
 var fs = require("fs")
 var path = require('path')
-var file = path.resolve(__dirname,'../config/setup.bat')
+var file = path.resolve(__dirname, '../config/setup.bat')
 add(file)
-function get(v,file){
-    return v.replace(/%~dp0/gi,path.join(path.parse(file).dir,'./'))
-            .replace(/%.*?%/gi,function(match){
-                return process.env[match.slice(1,match.length-1)]
-            })
+
+function get(v, file) {
+    return v.replace(/%~dp0/gi, path.join(path.parse(file).dir, './'))
+        .replace(/%.*?%/gi, function (match) {
+            return process.env[match.slice(1, match.length - 1)]
+        })
 }
-function add(file){
-         fs.readFileSync(file).toString()
-            .split(/[\r\n]+/g)
-            .filter(function(a){
-                //console.log(item_reg.test(a))
-               return item_reg.test(a)
-            })
-            .forEach(function(a){
-                let matches = a.match(item_reg)
-                let key = matches[1]
-                //console.log(key)
-                //console.log(matches[0])
-                //console.log(matches[2])
-                let value = get(matches[2],file)
-                process.env[key] = value
-                //console.log(value)
-            })
+
+function add(file) {
+    fs.readFileSync(file)
+        .toString()
+        .split(/[\r\n]+/g)
+        .filter(function (a) {
+            return item_reg.test(a)
+        })
+        .forEach(function (a) {
+            let matches = a.match(item_reg)
+            let key = matches[1]
+            let value = get(matches[2], file)
+            process.env[key] = value
+        })
 }
