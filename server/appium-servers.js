0742f894f48f55fb90c9a1a21c30336cf0806076
zhoushoujian <qianchengyimeng@qq.com>
2018-04-14 14:06:50 +0800

    增加双开端口

--- server/appium-servers.js
+++ server/appium-servers.js
@@ -1,8 +1,13 @@
 'use strict'
+let caps = require('../config/caps')
 exports.local = {
   host: 'localhost',
-  port: 4723
+  port: caps.android18.port
 };
+exports.remote = {
+  host: 'localhost',
+  port: caps.android19.port
+}
 console.log('初始化客户端端口环境   ok'.green)
 exports.sauce = {
   host: 'ondemand.saucelabs.com',
